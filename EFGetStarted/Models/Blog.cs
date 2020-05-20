ad351632806a80bcbcca86c2bfe6a86f6a9b6009
Alejandro Guzman <>
2020-05-20 14:09:37 -0700

    create domain models

---
+++ EFGetStarted/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace EFGetStarted.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+}
